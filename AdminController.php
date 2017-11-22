d4e0333a2624e9a945eeb947538e80fb99e2af12
kaviyarasans14 <kaviyarasans14@gmail.com>
2017-11-22 14:12:59 +0530

    ADMIN Modified

--- AdminController.php
+++ AdminController.php
@@ -14,6 +14,7 @@ class AdminController extends Controller
      */
     public function showIndex()
     {
+        $view = "";
         return $this->render('admin/index.html.twig');
                                 
     }
