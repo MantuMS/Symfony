d9bd33d0ae548e9b35ee3ec8387420d72da5ea6a
kaviyarasans14 <33854622+kaviyarasans14@users.noreply.github.com>
2017-11-22 12:17:52 +0530

    Update AdminController.php

--- AdminController.php
+++ AdminController.php
@@ -14,6 +14,7 @@ class AdminController extends Controller
      */
     public function showIndex()
     {
+        $view = "Testing";
         return $this->render('admin/index.html.twig');
                                 
     }
