053fae0d2ee931944bd32b25c85455fa778ff3ab
kaviyarasans14 <kaviyarasans14@gmail.com>
2017-11-23 11:43:47 +0530

    Mobile Modified

--- AdminController.php
+++ AdminController.php
@@ -14,7 +14,7 @@ class AdminController extends Controller
      */
     public function showIndex()
     {
-        $view = "Testing";
+        $view = "Testing SD";
         return $this->render('admin/index.html.twig');
                                 
     }
