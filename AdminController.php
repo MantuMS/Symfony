03d8f7731ed5ca95d86b7f66ee5cdd78824aeb57
MantuMS <mantuizcool@hotmail.com>
2017-12-22 15:55:27 +0530

    Mas

--- AdminController.php
+++ AdminController.php
@@ -14,7 +14,7 @@ class AdminController extends Controller
      */
     public function showIndex()
     {
-        $view = "Testing SD Support";
+        $view = "Testing ";
         return $this->render('admin/index.html.twig');
                                 
     }
